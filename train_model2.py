f2f70f4db75f0b362e07f071e3a468460d787cb5
Matson <madison.k.adams@gmail.com>
2026-04-15 10:00:57 -0400

    Update train_model2.py

--- train_model2.py
+++ train_model2.py
@@ -230,7 +230,7 @@ model.to(device)
 
 # Define optimizer
 params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.AdamW(params, lr=0.0005, momentum=0.9, weight_decay=0.0001)
+optimizer = torch.optim.AdamW(params, lr=0.0005, weight_decay=0.0001)
 
 # -------- RESUME FROM CHECKPOINT --------
 checkpoint_path = "best_fasterrcnn_mushroom_FULL.pth"
